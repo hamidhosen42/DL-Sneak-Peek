03c1a2288e5e273a861adac0cb758332795c320d
Md.Hamid Hosen <68488154+hamidhosen42@users.noreply.github.com>
2023-08-08 10:07:18 +0600

    Update basic.py

--- basic.py
+++ basic.py
@@ -20,3 +20,15 @@ Pipeline(steps=[('standardscaler', StandardScaler()),
 >>> clf.fit(X, y)
 Pipeline(steps=[('standardscaler', StandardScaler()),
                 ('svc', SVC(gamma='auto'))])
+
+
+>>> import numpy as np
+>>> from sklearn.pipeline import make_pipeline
+>>> from sklearn.preprocessing import StandardScaler
+>>> X = np.array([[-1, -1], [-2, -1], [1, 1], [2, 1]])
+>>> y = np.array([1, 1, 2, 2])
+>>> from sklearn.svm import SVC
+>>> clf = make_pipeline(StandardScaler(), SVC(gamma='auto'))
+>>> clf.fit(X, y)
+Pipeline(steps=[('standardscaler', StandardScaler()),
+                ('svc', SVC(gamma='auto'))])
